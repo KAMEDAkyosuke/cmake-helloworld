824fbc401fbec4a510ece0fdb92a9a39f52d0af1
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2013-03-01 15:35:48 +0900

    add subdir

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+#include "subdir/subdir.h"
+
 int main(int argc, char** argv)
 {
     puts("HELLO CMAKE WORLD");
+    func1();
     return 0;
 }
---
+++ subdir/subdir.h
@@ -0,0 +1,6 @@
+#ifndef SUBDIR_H
+#define SUBDIR_H
+
+void func1();
+
+#endif
